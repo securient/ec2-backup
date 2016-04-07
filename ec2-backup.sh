e926947dbd8aa0731141fcb2ea47107bb26b2a65
Bipin Pandey <pandeybipin007@gmail.com>
2016-04-07 00:59:46 -0400

    Update ec2-backup.sh
    Developed the module for attaching and detaching a Volume to the newly created instance.
    Also modified the Calculate_Size module in order to calculate the size in GB as Volume accepts the space in GB.

--- ec2-backup.sh
+++ ec2-backup.sh
@@ -20,6 +20,11 @@ if [ -z "$EC2_BACKUP_FLAGS_SSH" ]
   exit 1
 fi
 
+#if [ -z  $keypair ] || [  -z $secgroup  ]; then
+       # echo "The keypair or the security group are empty please set them in EC2_BACKUP_FLAGS_AWS" 1>&2
+       # exit 1
+#fi
+
 instance=`aws ec2 run-instances --image-id ami-fce3c696 --instance-type t2.micro --key-name keyname --security-groups SN_2|grep INSTANCES|awk '{print $8}'`
 echo "New Instance-Id is $instance"
 location=`aws ec2 describe-instances --output text --instance-id $instance|grep PLACEMENT|awk '{print $2}'`
@@ -37,11 +42,10 @@ echo $aws_hostname
 echo "ssh -o StrictHostKeyChecking=no ${EC2_BACKUP_FLAGS_SSH} ubuntu@$aws_hostname"
 ssh -o StrictHostKeyChecking=no ${EC2_BACKUP_FLAGS_SSH} ubuntu@$aws_hostname
 }
-
+### Usage Function
 usage() {
 echo "ec2_backup [-h] [-m method] [-v volume-id] dir "
 }
-# Parameter selection based on parameter
 while getopts h:m:v: flag; do
   case "${flag}" in
    h) usage ;;
@@ -66,9 +70,49 @@ if [ ! -d "$directory" ]
                 exit 1
 fi
 
-#To compute the size of directory and assign to directory
-#Calculate_Size()
-#{
-#size= du -sb $directory | awk '{print $1}' #generates size in bytes
-#new_vol= size * 2
-#}
+#To compute the size of directory in GB and then assign to Volume
+Calculate_Size()
+{
+size=$(du -sb $directory|awk '{print $1}')
+new_vol=`expr $size \* 2`
+gb=`expr 1024 \* 1024 \* 1024`
+new_vol_gb=`expr $new_vol / $gb`
+echo $new_vol_gb
+if [ $new_vol_gb -lt 1 ]
+then
+new_vol_gb=1
+else
+new_vol_gb= $new_vol_gb
+fi
+}
+
+Volume_Attach()
+{
+new_volume_id=`aws ec2 create-volume --output text --availability-zone $location --size $new_vol_gb|awk '{print $6}'` >/dev/null
+sleep 10
+aws ec2 attach-volume --volume-id $new_volume_id --instance-id $instance --device $device_map > /dev/null
+echo $new_volume_id
+sleep 05
+volsize=`aws ec2 describe-volumes --output text --volume-ids $new_volume_id|awk '{print $4}'`
+echo "The size is: $volsize"
+volstatus=`aws ec2 describe-volumes --output text --volume-ids $new_volume_id|awk '{print $6}'`
+echo "The Status is : $volstatus"
+}
+
+Volume_Detach()
+{
+aws ec2 detach-volume --volume-id $new_volume_id --output text >/dev/null
+echo "Volume $new_volume_id has been detached"
+}
+Terminate_Instance()
+{
+aws ec2 terminate-instances --instance-ids $instance >/dev/null
+echo "Instance Terminated"
+}
+
+Instance_Creation
+Calculate_Size
+Volume_Attach
+Volume_Detach
+Terminate_Instance
+echo $new_volume_id
